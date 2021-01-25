71ab1a111112b62b673b5fc800381a3f880fe43d
Akutra <psychokinesis@leapmaker.com>
2021-01-25 23:33:55 +0000

    Updated hello message - added more specifics - updated unit test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "*************\nIf you are seeing this then you have successfully build my-app\n- Into a docker container\n- via a Jenkins pipeline\nHello World!\n*************";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEqual(true, outContent.toString().contains("Hello World!"));
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" does not contain \"Hello World!\"");
         }
     }
 
