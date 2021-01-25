a3688c79827bc408881b2b1f043b9c9a1229448b
Akutra <psychokinesis@leapmaker.com>
2021-01-25 07:06:03 +0000

    Added Java Install to docker image

--- gdockerfile.sh
+++ gdockerfile.sh
@@ -2,6 +2,7 @@
 name="`mvn help:evaluate -Dexpression=project.name | grep '^[^\[]'`"
 version="`mvn help:evaluate -Dexpression=project.version | grep '^[^\[]'`"
 echo "FROM ubuntu:latest" > Dockerfile
+echo "RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install default-jre-headless && apt-get clean && rm -rf /var/lib/apt/lists/*" >> Dockerfile
 echo "WORKDIR /usr/src/app" >> Dockerfile
 echo "COPY target/*.jar ." >> Dockerfile
 echo "CMD [\"java\", \"-jar\", \"target/$name-$version.jar\"]" >> Dockerfile
