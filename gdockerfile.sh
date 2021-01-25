7c32e68b6b54704116390ec585473140310413d4
Akutra <psychokinesis@leapmaker.com>
2021-01-25 07:18:21 +0000

    Corrected path location of jar file

--- gdockerfile.sh
+++ gdockerfile.sh
@@ -5,4 +5,4 @@ echo "FROM ubuntu:latest" > Dockerfile
 echo "RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install default-jre-headless && apt-get clean && rm -rf /var/lib/apt/lists/*" >> Dockerfile
 echo "WORKDIR /usr/src/app" >> Dockerfile
 echo "COPY target/*.jar ." >> Dockerfile
-echo "CMD [\"java\", \"-jar\", \"target/$name-$version.jar\"]" >> Dockerfile
+echo "CMD [\"java\", \"-jar\", \"/usr/src/app/$name-$version.jar\"]" >> Dockerfile
