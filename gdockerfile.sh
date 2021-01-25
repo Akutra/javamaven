88fac84bdd7ac4a1c71c4c93d37976f23886682d
Akutra-Ramses Atenosis Cea <psychokinesis@leapmaker.com>
2021-01-24 21:44:40 -0800

    corrected typo in gdockerfile.sh

--- gdockerfile.sh
+++ gdockerfile.sh
@@ -4,4 +4,4 @@ VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 echo "FROM ubuntu:latest" > Dockerfile
 echo "WORKDIR /usr/src/app" >> Dockerfile
 echo "COPY target/*.jar ." >> Dockerfile
-echo 'CMD ["java", "-jar", "target/${NAME}-${VERSION}.jar"]' >> Dockerfile
+echo 'CMD ["java", "-jar", "target/$NAME-$VERSION.jar"]' >> Dockerfile
