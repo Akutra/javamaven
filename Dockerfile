1daf02bcfee191a01e86d1281b53ab22c71ec529
Akutra <psychokinesis@leapmaker.com>
2021-02-11 08:10:53 +0000

    Corrected Typo in gdockerfile script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM ubuntu:latest
 RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install default-jre-headless && apt-get clean && rm -rf /var/lib/apt/lists/*
 WORKDIR /usr/src/app
-COPY target/*.jar .
-CMD ["java", "-jar", "/usr/src/app/my-app-2.0.jar"]
+COPY target/myapp-2.1.jar .
+CMD ["java", "-jar", "/usr/src/app/myapp-2.1.jar"]
--- gdockerfile.sh
+++ gdockerfile.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-name="`mvn help:evaluate -Dexpression=project.name | grep '^[^\[]'`"
-version="`mvn help:evaluate -Dexpression=project.version | grep '^[^\[]'`"
+name="$(grep -oPm1 "(?<=<name>)[^<]+" pom.xml)"
+version="$(grep -oPm1 "(?<=<version>)[^<]+" pom.xml)"
+jarlib="target/$name-$version.jar"
 echo "FROM ubuntu:latest" > Dockerfile
 echo "RUN apt-get update && DEBIAN_FRONTEND=noninteractive apt-get -y install default-jre-headless && apt-get clean && rm -rf /var/lib/apt/lists/*" >> Dockerfile
 echo "WORKDIR /usr/src/app" >> Dockerfile
-echo "COPY target/*.jar ." >> Dockerfile
+echo "COPY $jarlib ." >> Dockerfile
 echo "CMD [\"java\", \"-jar\", \"/usr/src/app/$name-$version.jar\"]" >> Dockerfile
